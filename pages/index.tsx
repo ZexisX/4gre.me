4cf027bafaab9cd2b3d507b8a41e7af41d330f75
Aedotris ⛧ <127677696+levinhkhangzz@users.noreply.github.com>
2024-05-20 10:25:18 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
 import { useEffect, useState } from 'react'
-
 import { generateJSXMeshGradient } from 'meshgrad'
-
 import Container from 'components/Container'
 import cn from 'lib/classNames'
-
 import packageJSON from '../../meshgrad/package.json'
 
 const ELEMENTS = 6
@@ -13,6 +10,7 @@ export default function Home() {
   const [isServer, setIsServer] = useState(true)
   const [history, setHistory] = useState([generateJSXMeshGradient(ELEMENTS)])
   const [index, setIndex] = useState(0)
+  const [isLoaded, setIsLoaded] = useState(false) // State to track if the component is loaded
 
   const handleNewGradient = () => {
     setIndex(history.length)
@@ -21,11 +19,19 @@ export default function Home() {
 
   useEffect(() => {
     setIsServer(false)
+    // Set a timeout to update isLoaded after a delay
+    const timeout = setTimeout(() => setIsLoaded(true), 500)
+    // Clean up the timeout to prevent memory leaks
+    return () => clearTimeout(timeout)
   }, [])
 
   return (
     <Container>
-      <div className='relative inset-0 flex flex-col items-center justify-center gap-4'>
+      <div
+        className={`relative inset-0 flex flex-col items-center justify-center gap-4 ${
+          isLoaded ? 'fade-in' : ''
+        }`}
+      >
         <VersionBadge />
         <h1 className='font-semibold tracking-tighter text-7xl whitespace-nowrap'>
           DOMAIN | KHANG.AI
@@ -117,51 +123,6 @@ function Footer() {
         </span>
       </a>
     </p>
-  )
-}
-
-function CopiedIcon() {
-  return (
-    <svg
-      width='16'
-      height='16'
-      strokeWidth='1.5'
-      viewBox='0 0 24 24'
-      fill='none'
-      xmlns='http://www.w3.org/2000/svg'
-    >
-      <path
-        d='M5 13L9 17L19 7'
-        stroke='currentColor'
-        strokeLinecap='round'
-        strokeLinejoin='round'
-      />
-    </svg>
-  )
-}
-
-function CopyIcon() {
-  return (
-    <svg
-      width='16'
-      height='16'
-      strokeWidth='1.5'
-      viewBox='0 0 24 24'
-      fill='none'
-    >
-      <path
-        d='M19.4 20H9.6C9.26863 20 9 19.7314 9 19.4V9.6C9 9.26863 9.26863 9 9.6 9H19.4C19.7314 9 20 9.26863 20 9.6V19.4C20 19.7314 19.7314 20 19.4 20Z'
-        stroke='currentColor'
-        strokeLinecap='round'
-        strokeLinejoin='round'
-      />
-      <path
-        d='M15 9V4.6C15 4.26863 14.7314 4 14.4 4H4.6C4.26863 4 4 4.26863 4 4.6V14.4C4 14.7314 4.26863 15 4.6 15H9'
-        stroke='currentColor'
-        strokeLinecap='round'
-        strokeLinejoin='round'
-      />
-    </svg>
   )
 }
 
